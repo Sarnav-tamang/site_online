984bd4105f1ae5ad27b52a17454d79059ed9f527
Sarnav-tamang <117902610+Sarnav-tamang@users.noreply.github.com>
2024-12-27 11:42:48 +0530

    Add files via upload

---
+++ foolove.js
@@ -0,0 +1,6 @@
+let kill = document.getElementById("FOOL");
+let beach = document.getElementById("loophole1");
+
+beach.addEventListener('mouseover',function(){
+kill.textContent = "You are very Naughty HENRY!";
+});
